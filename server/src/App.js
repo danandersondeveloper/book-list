499d6b983fbe9e3c96a82ca91b4c2d7d2afc628f
dananderson <blusplatt@gmail.com>
2024-01-30 11:53:04 +0200

    Install middlewares and configure

--- server/src/App.js
+++ server/src/App.js
@@ -1,14 +1,25 @@
+const dotenv = require('dotenv').config();
 const express = require('express');
+const morgan = require('morgan');
+const helmet = require('helmet');
 
 const app = express();
+
+
+// Middlewares used on the server
+
+app.use(morgan('common'));		// Used for server loggin
+app.use(helmet());				// Used for basic server security
+
+
+// Configured requests used on the server
 
 app.get('/', (req, res) => {
 	res.status(200).json({ message: 'Booklist Server'});
 });
 
-
 app.get('/*', (req, res) => {
 	res.status(404).json({message: 'Request path could not be found!'});
 });
 
-app.listen(9000, () => console.log(`Booklist Server: listening on port 5000`));
+app.listen(process.env.PORT, () => console.log(`Booklist Server: listening on port ${process.env.PORT}`));
