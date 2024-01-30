408600c708320bb19afb3c7e29abe1048e0f9fc0
dananderson <blusplatt@gmail.com>
2024-01-30 11:35:14 +0200

    Add a 404 request handler to server

--- server/src/App.js
+++ server/src/App.js
@@ -6,4 +6,9 @@ app.get('/', (req, res) => {
 	res.status(200).json({ message: 'Booklist Server'});
 });
 
+
+app.get('/*', (req, res) => {
+	res.status(404).json({message: 'Request path could not be found!'});
+});
+
 app.listen(9000, () => console.log(`Booklist Server: listening on port 5000`));
