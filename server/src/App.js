29040d68c837a5beedbf29ed74f53c3e7be2c89f
dananderson <blusplatt@gmail.com>
2024-01-30 15:33:46 +0200

    User Controllers work and functionality

--- server/src/App.js
+++ server/src/App.js
@@ -2,16 +2,20 @@ const dotenv = require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
 const helmet = require('helmet');
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.DB_CONNECTION_STRING);
+
+// Route imports
 const users = require('./Routes/UserRoutes')
 
 const app = express();
 
-
 // Middlewares used on the server
 
+app.use(express.json());
 app.use(morgan('common'));		// Used for server loggin
 app.use(helmet());				// Used for basic server security
-
 
 // Configured requests used on the server
 
@@ -25,4 +29,9 @@ app.get('/*', (req, res) => {
 	res.status(404).json({message: 'Request path could not be found!'});
 });
 
-app.listen(process.env.PORT, () => console.log(`Booklist Server: listening on port ${process.env.PORT}`));
+mongoose.connection.once('open', () => {
+
+	console.log('DB Connected: book-list');
+
+	app.listen(process.env.PORT, () => console.log(`Booklist Server: listening on port ${process.env.PORT}`));
+})
--- server/src/Controllers/UserController.js
+++ server/src/Controllers/UserController.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const bcrypt = require('bcrypt');
+const User = require('../Models/UserModel');
 
 
 // @desc Get all users
@@ -6,7 +8,16 @@ const express = require('express');
 // @access Private
 
 const getUsers = async (req, res) => {
-	res.status(200).json({ message: 'Made a request to find users' });
+	try {
+		const users = await User.find().select('-password');
+
+		if (users.length <= 0) return res.status(502).json({ message: '502 Bad Gateway: No data found!'});
+		return res.json(users);
+
+	} catch(err) {
+		console.log(err);
+		return res.status(500).json({ message: '500 Internal Server Error' });
+	}
 }
 
 
@@ -15,7 +26,27 @@ const getUsers = async (req, res) => {
 // @access Private
 
 const createUser = async (req, res) => {
-	res.status(200).json({ message: 'Made a request to create a new user' })
+	try {
+		const { email, password } = req.body;
+
+		if (!email || !password) return res.status(400).json({ message: 'All fields are required!' });
+		if (await User.findOne({ email }).lean().exec()) return res.status(409).json({ message: 'User all ready exists'});
+
+		const saltRounds = 10;
+		const encryptedPassword = await bcrypt.hash(password, saltRounds);
+
+		const newUser = {
+			'email': email,
+			'password': encryptedPassword	
+		}
+
+		await User.create(newUser);
+		return res.status(200).json({ message: `New user created!`});
+		
+	} catch(err) {
+		console.log(err);
+		return res.status(500).json({ message: '500 Internal Error' });
+	}
 }
 
 
