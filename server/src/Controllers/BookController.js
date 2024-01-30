28b7f2040cd599a80b74813edf3cf5c46a7acb49
dananderson <blusplatt@gmail.com>
2024-01-30 16:21:01 +0200

    Update book controller

---
+++ server/src/Controllers/BookController.js
@@ -0,0 +1,65 @@
+const express = required('express');
+
+
+// @desc Get all users
+// @route GET /users
+// @access Private
+
+const getBooks = async (req, res) => {
+	try {
+
+		// Add functionality
+
+	} catch(err) {
+		console.log(err);
+		return res.status(500).json({ message: '500 Internal Server Error' })
+	}
+};
+
+
+// @desc Get all users
+// @route GET /users
+// @access Private
+
+const createBook = async (req, res) => {
+	try {
+
+		// Add functionality
+
+	} catch(err) {
+		console.log(err);
+		return res.status(500).json({ message: '500 Internal Server Error' })
+	}
+};
+
+
+// @desc Get all users
+// @route GET /users
+// @access Private
+
+const updateBook = async (req, res) => {
+	try {
+
+		// Add functionality
+
+	} catch(err) {
+		console.log(err);
+		return res.status(500).json({ message: '500 Internal Server Error' })
+	}
+}
+
+
+// @desc Get all users
+// @route GET /users
+// @access Private
+
+const deleteBook = async (req, res) => {
+	try {
+
+		// Add functionality
+
+	} catch(err) {
+		console.log(err);
+		return res.status(500).json({ message: '500 Internal Server Error' })
+	}
+}
